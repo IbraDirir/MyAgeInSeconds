9a2245d30d9e9bfbe994a6acc58b42ffe9fd7322
IbraDirir <khaliildirir@gmail.com>
2016-03-17 07:37:07 +0300

    gues myAge In Seconds

---
+++ age.js
@@ -0,0 +1,8 @@
+var secondsPerMinute = 60;
+var minutesPerHour = 60;
+var hoursPerDay = 24;
+var daysPerWeek = 7;
+var weeksPerYear = 52;
+var myAge = 22;
+var myAgeInSeconds = secondsPerMinute * minutesPerHour * hoursPerDay * daysPerWeek * weeksPerYear * myAge;
+document.write(myAge);
